bcc3d0b66b7a625bf37daafe8f01e909d00a8b9f
Eshwar K <esh.eshwar.eshwar.esh@gmail.com>
2025-04-28 13:12:43 -0400

    gpu run configs

--- policy/run.sh
+++ policy/run.sh
@@ -1,5 +1,5 @@
-if [ $# -ne 1 ]; then 
-    echo "missing experiment id parameter usage\n ./run.sh exp_id=<experiment_id>"
+if [ $# -ne 2 ]; then 
+    echo "missing experiment id parameter usage\n ./run.sh task_name=<task_name> exp_id=<experiment_id>"
 else
-    python train_tdmpc.py reward_type=dense agent=tdmpc experiment=tdmpc task_name=LunarLanderContinuous-v3 $1
+    python train_tdmpc.py reward_type=dense agent=tdmpc experiment=tdmpc $1 $2
 fi
